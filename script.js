a6d1271b0899f467b8132c4452e9c7bcfc65b247
EvaRoets <eva_roets@hotmail.com>
2021-06-29 12:08:04 +0200

    Display 1d weather

--- script.js
+++ script.js
@@ -15,10 +15,17 @@ let weather = {
     },
     displayWeather: function(data){
         const {name} = data;
-        const {temp } = data.main;
+        const {temp, humidity} = data.main;
         const {icon, description} = data.weather[0];
         const {speed} = data.wind;
         console.log(name,description,temp,icon,speed);
+        document.querySelector(".city").innerText = "This week's weather in " + name;
+        document.querySelector(".temp").innerText =  temp.toFixed(1) + "°C";
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description[0].toUpperCase() + description.slice(1);
+        document.querySelector(".humidity").innerText = humidity + "% humidity";
+        document.querySelector(".speed").innerText = (speed*3.6).toFixed(1)  + "km/h wind";
+
     }
 }
 
