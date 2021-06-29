da246034e12ae7d38a8906716ae192eb944131b1
EvaRoets <eva_roets@hotmail.com>
2021-06-29 10:48:18 +0200

    Add Fetch API

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 // grab form submit (mouse click or enter key)
 // stop form submitting
+
 // fetch API weather
+let weather = {
+    apiKey: "9c1a9692c7257945806ed5ade3b54eb0",
+    fetchWeather: function (city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
+        .catch((error) => console.log(error))
+    },
+    displayWeather: function(data){
+        const {name} = data;
+        const {temp } = data.main;
+        const {icon, description} = data.weather[0];
+        const {speed} = data.wind;
+        console.log(name,description,temp,icon,speed);
+    }
+}
+
+
+
+
+
 
