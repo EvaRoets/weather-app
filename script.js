3d00f50a44dc2fc8fe322c753db0effee88f346f
EvaRoets <eva_roets@hotmail.com>
2021-07-01 10:24:53 +0200

    Add complete 5day display

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let weather = {
             + "&cnt=5&units=metric&appid="
             + this.apiKey)
         .then((response) => response.json())
-        .then((data) => this.displayWeather(data, tempCardID))
+        .then((data) => this.displayWeather(data))
         .catch((error) => console.log(error))
     },
     //display weather
@@ -38,31 +38,80 @@ let weather = {
     //     document.querySelector(".speed").innerText = (speed*3.6).toFixed(1)  + "km/h wind";
 
     // 5 day display
-    displayWeather: function (data, tempCardID) {
+    displayWeather: function (data) {
+        let cityName = data.city.name
+        document.querySelector(".city").innerText = "This week's weather in " + cityName;
         // day one
-        let {name} = data;
-        let {temp, humidity} = data.list[0].main;
-        // let tempCardID =
-        document.querySelector(".city").innerText = "This week's weather in " + name;
-        document.getElementById(tempCardID).querySelector(".temp").innerText = temp.toFixed(1) + "°C";
+        let icon1 = data.list[0].weather[0].icon
+        let des1 = data.list[0].weather[0].description
+        let temp1 = data.list[0].main.temp
+        let hum1 = data.list[0].main.humidity
+        let speed1 = data.list[0].wind.speed
 
-        // let {icon, description} = data.list[0].weather[0];
-        // let {speed} = data.list[0].wind;
-        // document.getElementById(tempCardID).querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        // document.getElementById(tempCardID).querySelector(".description").innerText = description[0].toUpperCase() + description.slice(1);
-        // document.getElementById(tempCardID).querySelector(".humidity").innerText = humidity + "% humidity";
-        // document.getElementById(tempCardID).querySelector(".speed").innerText = (speed * 3.6).toFixed(1) + "km/h wind";
+        document.getElementById("card1").querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon1 + ".png";
+        document.getElementById("card1").querySelector(".description").innerText = des1[0].toUpperCase() + des1.slice(1);
+        document.getElementById("card1").querySelector(".temp").innerText = temp1.toFixed(1) + "°C";
+        document.getElementById("card1").querySelector(".humidity").innerText = hum1 + "% humidity";
+        document.getElementById("card1").querySelector(".speed").innerText = (speed1 * 3.6).toFixed(1) + "km/h wind";
 
         // day two
+        let icon2 = data.list[1].weather[0].icon
+        let des2 = data.list[1].weather[0].description
+        let temp2 = data.list[1].main.temp
+        let hum2 = data.list[1].main.humidity
+        let speed2 = data.list[1].wind.speed
 
+        document.getElementById("card2").querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon2 + ".png";
+        document.getElementById("card2").querySelector(".description").innerText = des2[0].toUpperCase() + des1.slice(1);
+        document.getElementById("card2").querySelector(".temp").innerText = temp2.toFixed(1) + "°C";
+        document.getElementById("card2").querySelector(".humidity").innerText = hum2 + "% humidity";
+        document.getElementById("card2").querySelector(".speed").innerText = (speed2 * 3.6).toFixed(1) + "km/h wind";
 
+        // day three
+        let icon3 = data.list[2].weather[0].icon
+        let des3 = data.list[2].weather[0].description
+        let temp3 = data.list[2].main.temp
+        let hum3 = data.list[2].main.humidity
+        let speed3 = data.list[2].wind.speed
+
+        document.getElementById("card3").querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon3 + ".png";
+        document.getElementById("card3").querySelector(".description").innerText = des3[0].toUpperCase() + des3.slice(1);
+        document.getElementById("card3").querySelector(".temp").innerText = temp3.toFixed(1) + "°C";
+        document.getElementById("card3").querySelector(".humidity").innerText = hum3 + "% humidity";
+        document.getElementById("card3").querySelector(".speed").innerText = (speed3 * 3.6).toFixed(1) + "km/h wind";
+
+        // day four
+        let icon4 = data.list[3].weather[0].icon
+        let des4 = data.list[3].weather[0].description
+        let temp4 = data.list[3].main.temp
+        let hum4 = data.list[3].main.humidity
+        let speed4 = data.list[3].wind.speed
+
+        document.getElementById("card4").querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon4 + ".png";
+        document.getElementById("card4").querySelector(".description").innerText = des4[0].toUpperCase() + des4.slice(1);
+        document.getElementById("card4").querySelector(".temp").innerText = temp4.toFixed(1) + "°C";
+        document.getElementById("card4").querySelector(".humidity").innerText = hum4 + "% humidity";
+        document.getElementById("card4").querySelector(".speed").innerText = (speed4 * 3.6).toFixed(1) + "km/h wind";
+
+        // day five
+        let icon5 = data.list[4].weather[0].icon
+        let des5 = data.list[4].weather[0].description
+        let temp5 = data.list[4].main.temp
+        let hum5 = data.list[4].main.humidity
+        let speed5 = data.list[4].wind.speed
+
+        document.getElementById("card5").querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon5 + ".png";
+        document.getElementById("card5").querySelector(".description").innerText = des5[0].toUpperCase() + des5.slice(1);
+        document.getElementById("card5").querySelector(".temp").innerText = temp5.toFixed(1) + "°C";
+        document.getElementById("card5").querySelector(".humidity").innerText = hum5 + "% humidity";
+        document.getElementById("card5").querySelector(".speed").innerText = (speed5 * 3.6).toFixed(1) + "km/h wind";
         },
     searchCity: function () {
         // get form value
         let cityInput = document.getElementById("location").value
         //get weather for form value
         this.fetchWeather(cityInput);
-    }
+    },
 };
 
 // link search to button
@@ -80,57 +129,6 @@ document.getElementById("location").addEventListener("keypress", function (event
     }
 });
 
-
-// // day two
-// {temp, humidity} = data.list[1].main;
-// {icon, description} = data.list[1].weather[0];
-// {speed} = data.list[1].wind;
-// console.log(name,description,temp,icon,speed);
-//
-// let temp2 = document.getElementById("temp2")
-// let icon2 = document.getElementById("icon2")
-// let des2 = document.getElementById("des2")
-// let hum2 = document.getElementById("hum2")
-// let speed2 = document.getElementById("speed2")
-//
-// temp2.innerText =  temp.toFixed(1) + "°C";
-// icon2.src = "http://openweathermap.org/img/wn/" + icon + ".png";
-// des2.innerText = description[0].toUpperCase() + description.slice(1);
-// hum2.innerText = humidity + "% humidity";
-// speed2.innerText = (speed*3.6).toFixed(1)  + "km/h wind";
-//
-// // day three
-// const {temp, humidity} = data.list[2].main;
-// const {icon, description} = data.list[2].weather[0];
-// const {speed} = data.list[2].wind;
-// console.log(name,description,temp,icon,speed);
-// document.getElementById("temp3").innerText =  temp.toFixed(1) + "°C";
-// document.getElementById("icon3").src = "http://openweathermap.org/img/wn/" + icon + ".png";
-// document.getElementById("des3").innerText = description[0].toUpperCase() + description.slice(1);
-// document.getElementById("hum3").innerText = humidity + "% humidity";
-// document.getElementById("speed3").innerText = (speed*3.6).toFixed(1)  + "km/h wind";
-//
-// // day four
-// const {temp, humidity} = data.list[3].main;
-// const {icon, description} = data.list[3].weather[0];
-// const {speed} = data.list[3].wind;
-// console.log(name,description,temp,icon,speed);
-// document.getElementById("temp4").innerText =  temp.toFixed(1) + "°C";
-// document.getElementById("icon4").src = "http://openweathermap.org/img/wn/" + icon + ".png";
-// document.getElementById("des4").innerText = description[0].toUpperCase() + description.slice(1);
-// document.getElementById("hum4").innerText = humidity + "% humidity";
-// document.getElementById("speed4").innerText = (speed*3.6).toFixed(1)  + "km/h wind";
-//
-// // day five
-// const {temp, humidity} = data.list[4].main;
-// const {icon, description} = data.list[4].weather[0];
-// const {speed} = data.list[4].wind;
-// console.log(name,description,temp,icon,speed);
-// document.getElementById("temp5").innerText =  temp.toFixed(1) + "°C";
-// document.getElementById("icon5").src = "http://openweathermap.org/img/wn/" + icon + ".png";
-// document.getElementById("des5").innerText = description[0].toUpperCase() + description.slice(1);
-// document.getElementById("hum5").innerText = humidity + "% humidity";
-// document.getElementById("speed5").innerText = (speed*3.6).toFixed(1)  + "km/h wind";
 
 
 // TODO: when search bar empty = no cards/displayed days
